c3922ca5cc86d294ca0ef5f96ada832a23dcebfb
Harrie Smulders <h.l.w.smulders@tue.nl>
2026-01-15 10:03:35 +0100

    10 Django REST Framework - Creating Data | ListCreateAPIView and Generic View Internals - Part 2

--- Video02/api/urls.py
+++ Video02/api/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListCreateAPIView.as_view()),
-    path('products/create/', views.ProductListCreateAPIView.as_view()),
     path('products/info/', views.ProductInfoAPIView.as_view()),
     path('products/<int:pk>', views.ProductDetailAPIView.as_view()),
     path('orders/', views.OrderListAPIView.as_view()),
